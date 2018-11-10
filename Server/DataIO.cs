11a05b0a779b112b23c2cfce6d83fbfd2b7fccc3
mmilica95 <milica095@gmail.com>
2018-11-10 10:48:18 +0100

    DataIO

---
+++ Server/DataIO.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Server
+{
+	public class DataIO
+	{
+		public void SerializeObject<T>(T serializableObject, string fileName)
+		{
+			if (serializableObject == null) { return; }
+			try
+			{
+				XmlDocument xmlDocument = new XmlDocument();
+				XmlSerializer serializer = new XmlSerializer(serializableObject.GetType());
+				using (MemoryStream stream = new MemoryStream())
+				{
+					serializer.Serialize(stream, serializableObject);
+					stream.Position = 0;
+					xmlDocument.Load(stream);
+					xmlDocument.Save(fileName);
+					stream.Close();
+				}
+			}
+			catch (Exception)
+			{ }
+		}
+
+
+		public T DeSerializeObject<T>(string fileName)
+		{
+			if (string.IsNullOrEmpty(fileName)) { return default(T); }
+
+			T objectOut = default(T);
+
+			try
+			{
+				string attributeXml = string.Empty;
+
+				XmlDocument xmlDocument = new XmlDocument();
+				xmlDocument.Load(fileName);
+				string xmlString = xmlDocument.OuterXml;
+
+				using (StringReader read = new StringReader(xmlString))
+				{
+					Type outType = typeof(T);
+
+					XmlSerializer serializer = new XmlSerializer(outType);
+					using (XmlReader reader = new XmlTextReader(read))
+					{
+						objectOut = (T)serializer.Deserialize(reader);
+						reader.Close();
+					}
+
+					read.Close();
+				}
+			}
+			catch (Exception)
+			{ }
+
+			return objectOut;
+		}
+
+		public bool IsFileLocked(FileInfo file)
+		{
+			FileStream stream = null;
+
+			try
+			{
+				stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.None);
+			}
+			catch (IOException)
+			{
+				//the file is unavailable because it is:
+				//still being written to
+				//or being processed by another thread
+				//or does not exist (has already been processed)
+				return true;
+			}
+			finally
+			{
+				if (stream != null)
+					stream.Close();
+			}
+
+			//file is not locked
+			return false;
+		}
+	}
+}
+
