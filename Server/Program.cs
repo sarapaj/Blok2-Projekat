e469febfab452fb8f831decbdb67f0907d5474e3
ininic <ininic95@gmail.com>
2018-11-14 19:03:30 +0100

    Ispravke

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,58 +1,77 @@
 ﻿using Common;
+using Manager;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
 using System.ServiceModel;
 using System.ServiceModel.Description;
+using System.ServiceModel.Security;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Server
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			//novi kod
-			ServiceHost host = null;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// srvCertCN.SubjectName should be set to the service's username. .NET WindowsIdentity class provides information about Windows user running the given process
+			string srvCertCN = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
+            Console.WriteLine("Ime " + WindowsIdentity.GetCurrent().Name);
+            NetTcpBinding binding = new NetTcpBinding();
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
+            string address = "net.tcp://localhost:9009/MainService";
 
-			host = new ServiceHost(typeof(MainService));
-			var binding = new NetTcpBinding();
-			binding.TransactionFlow = true;
-			host.AddServiceEndpoint(typeof(IMainService), binding, new Uri("net.tcp://localhost:9009/MainService"));
-			host.Description.Behaviors.Remove(typeof(ServiceDebugBehavior));
-			host.Description.Behaviors.Add(new ServiceDebugBehavior() { IncludeExceptionDetailInFaults = true });
-			host.Open();
-			Console.WriteLine("Server ready and waiting for requests.");
+            ServiceHost host = new ServiceHost(typeof(MainService));
+            host.AddServiceEndpoint(typeof(IMainService), binding, address);
 
-			///
-			//NetTcpBinding binding = new NetTcpBinding();
-   //         string address = "net.tcp://localhost:9009/MainService";
+            ///Custom validation mode enables creation of a custom validator - CustomCertificateValidator
+            //	host.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.Custom;
+            //  host.Credentials.ClientCertificate.Authentication.CustomCertificateValidator = new ServiceCertValidator();
 
-   //         ServiceHost host = new ServiceHost(typeof(MainService));
-   //         host.AddServiceEndpoint(typeof(IMainService), binding, address);
+            ///If CA doesn't have a CRL associated, WCF blocks every client because it cannot be validated
+            host.Credentials.ClientCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
 
-            //host.Description.Behaviors.Remove(typeof(ServiceDebugBehavior));
-            //host.Description.Behaviors.Add(new ServiceDebugBehavior() { IncludeExceptionDetailInFaults = true });
+            ///Set appropriate service's certificate on the host. Use CertManager class to obtain the certificate based on the "srvCertCN"
+            host.Credentials.ServiceCertificate.Certificate = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, srvCertCN);
+            /// host.Credentials.ServiceCertificate.Certificate = CertManager.GetCertificateFromFile("WCFService.pfx");
 
-           // host.Open();
 
-            //Console.WriteLine("SecurityService service is started.");
-            //Console.WriteLine("Press <enter> to stop service.");
+            host.Description.Behaviors.Remove(typeof(ServiceDebugBehavior));
+            host.Description.Behaviors.Add(new ServiceDebugBehavior() { IncludeExceptionDetailInFaults = true });
 
-			DataIO data = new DataIO();
+            try
+            {
+                host.Open();
+                Console.WriteLine("WCFService is started.\nPress <enter> to stop ...");
 
-            //Serijalizacija liste
-            List<Entity> lista = new List<Entity>(){
-            new Entity(1,1,"Novi Sad",DateTime.Now,3434),
-            new Entity(2,2,"Novi Sad",DateTime.Now,3489),
-            new Entity(3,3,"Novi Sad",DateTime.Now,4645)
-            };
-            data.SerializeObject<List<Entity>>(lista, "db.xml");
+                Console.WriteLine("SecurityService service is started.");
+                Console.WriteLine("Press <enter> to stop service.");
+                DataIO data = new DataIO();
 
-            Console.ReadLine();
-            host.Close();
+                //Serijalizacija liste
+                List<Entity> lista = new List<Entity>(){
+                new Entity(1,1,"Novi Sad",DateTime.Now,3434),
+                new Entity(2,2,"Novi Sad",DateTime.Now,3489),
+                new Entity(3,3,"Novi Sad",DateTime.Now,4645)
+                };
+                data.SerializeObject<List<Entity>>(lista, "db.xml");
+
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[ERROR] {0}", e.Message);
+                Console.WriteLine("[StackTrace] {0}", e.StackTrace);
+            }
+            finally
+            {
+                host.Close();
+            }
+
         }
-	}
+    }
 }
